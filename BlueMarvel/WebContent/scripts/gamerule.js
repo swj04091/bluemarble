206a909d7bdf6eb4095fd41a0bcb1a2ac7c13777
swj04 <swj04@LogCat2>
2019-11-10 17:53:27 +0900

    도시 구매 코드 수정 문제점: 도시를 구매하게 되면 내가 아닌  다음 사람의 돈이 차감됨.

--- BlueMarvel/WebContent/scripts/gamerule.js
+++ BlueMarvel/WebContent/scripts/gamerule.js
@@ -8,8 +8,39 @@ function moveUser(player, beforePoint, afterPoint, afterId, beforeId){
                $(gamer.afterId).append(html);
                $(gamer.beforeId).find(".btn-danger"+player).remove();
                gamer.beforePoint = gamer.afterPoint;
-               console.log("이동 전: "+beforeId);
-               console.log("이 동 후: "+afterId);
+               console.log("이동 전: "+gamer.beforeId);
+               console.log("이동 후: "+gamer.afterId);
            }
 
- 
+function doubleDice(ran1, ran2, player, allPlayer){
+	console.log("doubleDice 함수 실행 확인!");
+	console.log("allPlayer : ", allPlayer);
+	console.log("player : ", player);
+	// 플레이어 변경
+    if(ran1 != ran2){
+   	 	player++;
+   	 if(player>allPlayer){
+   		 console.log("allPlayer : ", allPlayer);
+       	 player = player-allPlayer;
+     }
+    } else {
+	   	 console.log("더블!");
+	   	 if(gamer.afterPoint == 11){
+	   		player++;
+	   		if(player>allPlayer){
+	      		 console.log("allPlayer : ", allPlayer);
+	          	 player = player-allPlayer;
+	        }
+	   	 }
+	   	 if(gamer.afterPoint == 31 || gamer.beforePoint == 31){
+	   		player++;
+	   		if(player>allPlayer){
+	      		 console.log("allPlayer : ", allPlayer);
+	          	 player = player-allPlayer;
+	        }
+	   	 }
+    }
+    console.log("doubleDice player : ", player);
+    return player;
+}
+
